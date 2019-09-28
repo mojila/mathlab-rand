52636c055478cf5e89d5b46014301ee5603dcdc6
mojila <mojila@gitlab.com>
2019-09-28 16:31:37 +0700

    fix

--- main.m
+++ main.m
@@ -1,9 +1,16 @@
 disp("Tugas 2 sub nomor 2");
 
-x = 1:10;
+function num = generate()
+    var2 = floor(rand(1)*10)
+    if var2 == 7 || var2 == 6 || var2 == 0
+        var2 = generate();
+    end
+    
+    num = var2;
+    return;
+end
 
-odd = rem(x,2) == 1 & x != 6 & x != 7;
+generated_number = generate();
 
-getOddNumbers = x(odd);
-
-getOddNumbers
+disp("Hasil Generator Angka antara 1-10 kecuali 6 dan 7");
+disp(generated_number);
